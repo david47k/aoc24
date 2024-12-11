d8ae4203fac4e8136fef6253a61417a1a8eef3f0
david47k <67101285+david47k@users.noreply.github.com>
2024-12-11 18:25:34 +1100

    still slow

--- src/day11.rs
+++ src/day11.rs
@@ -5,55 +5,45 @@
 
 use itertools::Itertools;
 
-#[derive(Clone)]
-enum Space {
-    Stone(u32),
-    Stones(Vec<Space>),
-}
 pub fn day11(input: &String) {
     // read in numbers
-    let input: Vec<Space> = input.trim_end().split_whitespace().map(|s| s.parse::<u32>().expect("number")).map(|n| Space::Stone(n)).collect_vec();
-    let mut stones = Space::Stones(input);
+    let mut stones: Vec<u32> = input.trim_end().split_whitespace().map(|s| s.parse::<u32>().expect("number")).collect_vec();
+
     let mut blinks: usize = 0;
     let mut part1_count: usize = 0;
     let mut part2_count: usize = 0;
 
-    let v = flatten_v(&stones);
-    println!("{0} blinks, {1} stones", blinks, v.len());
+    let t0 = crate::time::get_time_ms();
+    let t1: f64 = 0_f64;
 
-    for b in 1..=75 {
-        blinks = b;
-        stones = apply_blink(stones);
-        let stone_count = count_stones(&stones);
-        if b % 5 == 0 {
-            println!("flattening...");
-            let v = flatten_v(&stones);
-            stones = Space::Stones(v.iter().map(|&n| Space::Stone(n)).collect_vec());
+    //println!("{0} blinks, {1} stones", blinks, v.len());
+
+    for i in 0..75 {
+        println!("depth {0}", i+1);
+        let len = stones.len();
+        for si in 0..len {
+            let extra = apply_blink(&mut stones[si]);
+            if let Some(x) = extra {
+                stones.push(x);
+            }
         }
-        if b == 25 {
-            part1_count = stone_count;
+        println!("stones {0}", stones.len());
+
+        if i == 24 {
+            part1_count = stones.len();
+            println!("part one: {0}", part1_count);
         }
-        if b == 75 {
-            part2_count = stone_count;
+        if i == 37 {
+            println!("--------------------\ntime: {0:4.3}s\n--------------------", (crate::time::get_time_ms() - t0)/1000_f64);
         }
-        println!("{0} blinks, {1} stones", blinks, stone_count);
+        if i == 74 {
+            part2_count = stones.len();
+        }
     }
+
     println!("part one: {0}", part1_count);
     println!("part two: {0}", part2_count);
 
-}
-fn flatten_v(sp: &Space) -> Vec<u32> {
-    match sp {
-        Space::Stones(v) => v.iter().map(|x| flatten_v(x)).flatten().collect_vec(),
-        Space::Stone(n) => vec![*n],
-    }
-}
-
-fn count_stones(sp: &Space) -> usize {
-    match sp {
-        Space::Stones(v) => v.iter().map(|x| count_stones(x)).sum(),
-        Space::Stone(_) => 1,
-    }
 }
 
 fn count_digits_odd(n: u32) -> bool {
@@ -88,23 +78,19 @@ fn count_digits_odd(n: u32) -> bool {
     return false;
 }
 
-fn apply_blink(sp: Space) -> Space {
-    match sp {
-        Space::Stones(v) => Space::Stones(v.into_iter().map(|x| apply_blink(x)).collect_vec()),
-        Space::Stone(n) => {
-
-            if n == 0 {
-                return Space::Stone(1);
-            } else if !count_digits_odd(n) {
-                let s = n.to_string();
-                let lh = s[0..s.len() / 2].parse::<u32>().expect("number");
-                let rh = s[s.len() / 2..s.len()].parse::<u32>().expect("number");
-                return Space::Stones(vec![Space::Stone(lh), Space::Stone(rh)]);
-            } else {
-                return Space::Stone(n * 2024);
-            }
-        }
+fn apply_blink(n: &mut u32) -> Option<u32> {
+    if *n == 0 {
+        *n = 1;
+        return None;
     }
+    if !count_digits_odd(*n) {
+        let s = n.to_string();
+        let lh = s[0..s.len() / 2].parse::<u32>().expect("number");
+        let rh = s[s.len() / 2..s.len()].parse::<u32>().expect("number");
+        *n = lh;
+        return Some(rh);
+    }
+    *n = *n * 2024;
+    return None;
 }
-
 
