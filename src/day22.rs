a67b1010290601761f819dc492aaf962736a2b92
david47k <67101285+david47k@users.noreply.github.com>
2025-01-08 12:35:31 +1100

    improve performance of day 22

--- src/day22.rs
+++ src/day22.rs
@@ -1,7 +1,5 @@
 use itertools::Itertools;
-//use std::collections::{*};
-//use crate::grid::{*};
-//use crate::vector::{*};
+use std::collections::{*};
 
 fn secret_number_step(mut input: u64) -> u64 {
 	let x = input * 64;
@@ -45,7 +43,7 @@ pub fn day22(input: &String) -> (String, String) {
 		all_numbers.push(numbers);
 		all_prices.push(prices);
 		all_diffs.push(diffs);
-		println!("{n}: {}", all_numbers[i][1999]);
+		//println!("{n}: {}", all_numbers[i][1999]);
 		p1result += all_numbers[i][1999];
 	}
 
@@ -53,24 +51,47 @@ pub fn day22(input: &String) -> (String, String) {
 
 	// part 2
 	// we can use all_numbers, all_prices, all_diffs from above
-	let mut best_pattern = &all_diffs[0][0..4];
-	let mut best_price: i64 = 0;
+
+	let t0 = crate::time::get_time_ms();
+
+	// calculate a price diff set, to speed things up
+	println!("calculating diff sets for part 2...");
+	let mut all_sets: Vec<Vec<[i8;4]>> = vec![];
+	let mut buyer_maps: Vec<BTreeMap<[i8;4],i32>> = vec![];
+	for buyer_idx in 0..all_prices.len() {
+		let mut buyer_map: BTreeMap<[i8;4],i32> = BTreeMap::new();
+		let mut set: Vec<[i8;4]> = vec![];
+		for i in 0..2000-4_usize {
+			let x = [ all_diffs[buyer_idx][i], all_diffs[buyer_idx][i+1], all_diffs[buyer_idx][i+2], all_diffs[buyer_idx][i+3] ];
+			set.push(x);
+			if !buyer_map.contains_key(&x) {
+				// save the price... which is all_prices[buyer_idx][i + 3];
+				buyer_map.insert(x, all_prices[buyer_idx][i+3] as i32);
+			}
+		}
+		all_sets.push(set);
+		buyer_maps.push(buyer_map);
+	}
+	println!("done!");
+
+	let mut best_pattern = all_sets[0][0];
+	let mut best_price: i32 = 0;
+
+	let mut tested_patterns: BTreeSet<[i8;4]> = BTreeSet::new();
+
 
 	for buyer_idx in 0..all_prices.len() {
 		println!("buyer: {buyer_idx}");
 		for i in 0..2000-4_usize {
-			let pattern = &all_diffs[buyer_idx][i..i + 4];
-			let mut price: i64 = 0;
+			let pattern = all_sets[buyer_idx][i];
+			if tested_patterns.contains(&pattern) {
+				continue;
+			}
+			tested_patterns.insert(pattern);
+			let mut price: i32 = 0;
 			for buyer_idx_2 in buyer_idx..all_prices.len() {
-				for j in 0..2000 - 4_usize {
-					// if i == 0 && j < 10 {
-					// 	println!("{:10}: {} {}", all_numbers[i][j], all_prices[i][j], all_diffs[i][j]);
-					// }
-
-					if *pattern == all_diffs[buyer_idx_2][j..j + 4] {
-						price += all_prices[buyer_idx_2][j + 3] as i64;
-						break;	// where the pattern FIRST occurs
-					}
+				if let Some(j) = buyer_maps[buyer_idx_2].get(&pattern) {
+					price += j;
 				}
 			}
 			if price > best_price {
@@ -78,14 +99,15 @@ pub fn day22(input: &String) -> (String, String) {
 				best_price = price;
 			}
 		}
-		println!("best price so far: {best_price}");
-		println!("best pattern so far: {:?}", best_pattern);
+		println!("best price so far: {:5}   {:?}", best_price, best_pattern);
 		if buyer_idx == 10 {
 			break;
 		}
 	}
 
-	println!("part 2 result: best price after maximum 10 buyers: {best_price}");
-	(p1result.to_string(), best_price.to_string())
+	let t1 = crate::time::get_time_ms();
 
+	println!("part 2 result: best price after maximum 10 buyers: {best_price}");
+	println!("part 2 time: {:.0} ms", t1 - t0);
+	(p1result.to_string(), best_price.to_string())
 }
